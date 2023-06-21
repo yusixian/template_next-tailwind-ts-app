992b4a3428d206c9e4fa7025c4b2875bd9d4a839
yusixian <3450414733@qq.com>
2023-06-21 20:21:08 +0800

    feat: navigator & theme & material

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
 import '../styles/globals.css';
- 
+
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -12,12 +12,10 @@ type NextPageWithLayout = NextPage & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
 
-  return ( 
-		<ThemeProvider attribute="class">{getLayout(<Component {...pageProps} />)}</ThemeProvider>
-    );
+  return <ThemeProvider attribute="class">{getLayout(<Component {...pageProps} />)}</ThemeProvider>;
 }
-
 export default App;
